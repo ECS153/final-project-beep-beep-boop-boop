0e2aa6633f11348d28c86a0499e29877daf59664
Ryan Chan <ryanchan.cch@gmail.com>
2020-05-23 21:01:43 -0700

    Fixed client key saving, added client encryption and decryption

--- app/static/main.js
+++ app/static/main.js
@@ -1,75 +1,68 @@
 let socket = io.connect('http://' + document.domain + ':' + location.port);
 var username;
 var nickname;
-var public_key;
-var private_key;
+var keys;
 var server_public_key = null;
 var online_users = null;
 
-//TODO: Check if key exists then import
 
-// console.log("KEYS EXIST, GONNA IMPORT")
-// window.crypto.subtle.importKey(
-//     "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
-//     {   //this is an example jwk key, other key types are Uint8Array objects
-//         kty: "RSA",
-//         e: "AQAB",
-//         n: "vGO3eU16ag9zRkJ4AK8ZUZrjbtp5xWK0LyFMNT8933evJoHeczexMUzSiXaLrEFSyQZortk81zJH3y41MBO_UFDO_X0crAquNrkjZDrf9Scc5-MdxlWU2Jl7Gc4Z18AC9aNibWVmXhgvHYkEoFdLCFG-2Sq-qIyW4KFkjan05IE",
-//         alg: "RSA-OAEP-256",
-//         ext: true,
-//     },
-//     {   //these are the algorithm options
-//         name: "RSA-OAEP",
-//         hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
-//     },
-//     false, //whether the key is extractable (i.e. can be used in exportKey)
-//     ["encrypt"] //"encrypt" or "wrapKey" for public key import or
-//                 //"decrypt" or "unwrapKey" for private key imports
-// )
-// .then(function(publicKey){
-//     //returns a publicKey (or privateKey if you are importing a private key)
-//     console.log(publicKey);
-// })
-// .catch(function(err){
-//     console.error(err);
-// });
+// EXAMPLE USE OF FUNCTIONS
+async function testEncryptDecrypt(){
+    console.log("Creating keys")
+    keys = await createKeys()
+    message = "hi bby"
+    encoded = await encodeMessage(message)
+    encrypted = await encryptData(encoded, keys.publicKey)
+    decrypted = await decryptData(encrypted, keys.privateKey)
+    decoded = await decodeMessage(decrypted)
+    console.log("Printing decrypted message")
+    console.log(decoded)
+}
 
-console.log("KEYS DO NOT EXIST, GONNA GENERATE")
-window.crypto.subtle.generateKey(
-    {
-        name: "RSA-OAEP",
-        modulusLength: 2048, //can be 1024, 2048, or 4096
-        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-        hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
-    },
-    true, //whether the key is extractable (i.e. can be used in exportKey)
-    ["encrypt", "decrypt"] //must be ["encrypt", "decrypt"] or ["wrapKey", "unwrapKey"]
-)
-.then(function(key){
-    //returns a keypair object
-    console.log(key);
-    console.log(key.publicKey);
-    console.log(key.privateKey);
-    public_key = key.public_key;
-    private_key = key.private_key;
-})
-.catch(function(err){
-    console.error(err);
-});
+function createKeys(){
+    return crypto.subtle.generateKey(
+        {
+            name: "RSA-OAEP",
+            modulusLength: 2048, //can be 1024, 2048, or 4096
+            publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+            hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+        },
+        false, //whether the key is extractable (i.e. can be used in exportKey)
+        ["encrypt", "decrypt"] //must be ["encrypt", "decrypt"] or ["wrapKey", "unwrapKey"]
+    )
+}
 
-// window.crypto.subtle.exportKey(
-//     "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
-//     key.publicKey //can be a publicKey or privateKey, as long as extractable was true
-// )
-// .then(function(keydata){
-//     //returns the exported key data
-//     console.log(keydata);
-// })
-// .catch(function(err){
-//     console.error(err);
-// });
+function encryptData(data, publicKey){
+    return crypto.subtle.encrypt(
+        {
+            name: "RSA-OAEP",
+            //label: Uint8Array([...]) //optional
+        },
+        publicKey, //from generateKey or importKey above
+        data //ArrayBuffer of data you want to encrypt
+    )
+}
 
+function decryptData(data, privateKey){
+    return crypto.subtle.decrypt(
+        {
+            name: "RSA-OAEP",
+            //label: Uint8Array([...]) //optional
+        },
+        privateKey, //from generateKey or importKey above
+        data //ArrayBuffer of the data
+    )
+}
 
+function encodeMessage(message){
+    let encoder = new TextEncoder();
+    return encoder.encode(message);
+}
+
+function decodeMessage(message){
+    let decoder = new TextDecoder();
+    return decoder.decode(message)
+}
 
 document.getElementById("submit").addEventListener("click", handle_login);
 
@@ -114,7 +107,7 @@ socket.on( 'message', function( msg ) {
 });
 
 
-function handle_login() {
+async function handle_login() {
     console.log("hello");
     login_card = document.getElementById("login_view").children[0];
     username = login_card.children[1].value;
@@ -123,7 +116,10 @@ function handle_login() {
     if (nickname == "") {
         nickname = username;
     }
-    console.log(public_key);
+    console.log("Creating keys")
+    keys = await createKeys()
+    console.log(keys.publicKey);
+
     if (username != "") {
         document.getElementById("nickname").innerHTML = nickname;
         color_strip = document.getElementsByClassName("color_strip")[0];

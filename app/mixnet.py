c0c0da09079cd6c19254b3c46b594e9541af908b
Ryan Chan <ryanchan.cch@gmail.com>
2020-06-04 23:15:20 -0700

    Added shuffle to mixnet

--- app/mixnet.py
+++ app/mixnet.py
@@ -6,12 +6,16 @@ import settings
 import json
 import sys
 import requests
+import collections
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = settings.APP_SECRET_KEY
 socketio = SocketIO(app, ping_interval=100000, ping_timeout=100000)
 key = Keys()
+post_msg_queue = collections.deque()
+sched = BackgroundScheduler()
 
 
 @app.route('/getServerPublicKey', methods=['GET'])
@@ -21,23 +25,30 @@ def get_public_key():
 
 @app.route('/handle_incoming_package', methods=['POST'])
 def handle_incoming_packageV2():
-    package = json.loads(request.get_data())
-    encoded = encode_array(package)
-    decrypted = json.loads(decrypt(encoded, key.getPrivateKey()))
-    print(decrypted)
-    url = 'http://' + decrypted['recipient'] + '/handle_incoming_package'
-    try:
-        requests.post(url, data=json.dumps(decrypted['encrypted']), timeout=0.0001)
-    except requests.exceptions.ReadTimeout:
-        pass
-    return 'Success'
+    package = json.loads(request.get_data())    
+    post_msg_queue.append(package)
+
+
+def send_queued_message():
+    random.shuffle(post_msg_queue)
+    while post_msg_queue:
+        package = post_msg_queue.popleft()
+        encoded = encode_array(package)
+        decrypted = json.loads(decrypt(encoded, key.getPrivateKey()))
+        print(decrypted)
+        url = 'http://' + decrypted['recipient'] + '/handle_incoming_package'
+        try:
+            requests.post(url, data=json.dumps(package['encrypted']), timeout=0.0001)
+        except requests.exceptions.ReadTimeout:
+            pass
 
 
 def main():
     if len(sys.argv) != 2:
         print("python3 mixnet.py <port>")
         exit()
-
+    sched.add_job(send_queued_message, 'interval', seconds=0.2)
+    sched.start()
     socketio.run(app, host='0.0.0.0', port=sys.argv[1], debug=settings.DEBUG_MODE)
 
 
